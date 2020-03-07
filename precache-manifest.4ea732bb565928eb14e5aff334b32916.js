9e4da87ce8ea8d04ed03e1c60cebeecc6c44ebd6
Rodobaldo Cupull <rcupull@gmail.com>
2020-03-07 18:36:27 -0500

    Updates

---
+++ precache-manifest.4ea732bb565928eb14e5aff334b32916.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a780980251a900d725939137e7eb10b",
+    "url": "/themoviedb/index.html"
+  },
+  {
+    "revision": "c42bbe3689806bcde8c5",
+    "url": "/themoviedb/static/css/2.6102cdf0.chunk.css"
+  },
+  {
+    "revision": "c42bbe3689806bcde8c5",
+    "url": "/themoviedb/static/js/2.e458cf8e.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/themoviedb/static/js/2.e458cf8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dcca4563c45a322292e",
+    "url": "/themoviedb/static/js/main.0ed1f1a3.chunk.js"
+  },
+  {
+    "revision": "f948a04f753574ffe9b3",
+    "url": "/themoviedb/static/js/runtime-main.53c788e9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/themoviedb/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/themoviedb/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/themoviedb/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/themoviedb/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/themoviedb/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
